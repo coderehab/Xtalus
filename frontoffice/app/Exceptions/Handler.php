9f19eba0345830e3ebc3726f410ed90af4849cee
Edgar Ravenhorst <edgarravenhorst@Edgars-iMac.local>
2015-08-24 19:46:20 +0200

    asd

--- frontoffice/app/Exceptions/Handler.php
+++ frontoffice/app/Exceptions/Handler.php
@@ -1,46 +1,6 @@
 <?php namespace App\Exceptions;
 
-use Exception;
-use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Exception; use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler; // use symfony\http-kernel\Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+class Handler extends ExceptionHandler { protected $dontReport = [ 'Symfony\Component\HttpKernel\Exception\HttpException' ]; public function report(Exception $e) { return parent::report($e); } public function render($request, Exception $e) { if ($this->isHttpException($e)) { return $this->renderHttpException($e); }else { return parent::render($request, $e); } }
 
-class Handler extends ExceptionHandler {
-
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        'Symfony\Component\HttpKernel\Exception\HttpException'
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $e
-     * @return void
-     */
-    public function report(Exception $e)
-    {
-        return parent::report($e);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $e)
-    {
-        if($e instanceof NotFoundHttpException)
-        {
-            return response()->view('app');
-        }
-        return parent::render($request, $e);
-    }
-
-}
+                                       }
