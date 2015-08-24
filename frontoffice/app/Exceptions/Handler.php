01a4b36b60e35cb39a3434749087b7717b3b865d
Edgar Ravenhorst <edgarravenhorst@Edgars-iMac.local>
2015-08-24 19:30:07 +0200

    adas

--- frontoffice/app/Exceptions/Handler.php
+++ frontoffice/app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 use Exception;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler {
 
@@ -36,11 +37,10 @@ class Handler extends ExceptionHandler {
      */
     public function render($request, Exception $e)
     {
-        if($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
-            return view('home');
-        }else{
-            return parent::render($request, $e);
+        if($e instanceof NotFoundHttpException){
+            return view('errors.404');
         }
+        return parent::render($request, $e);
     }
 
 }
