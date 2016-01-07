cf82f43ed84146efbba6d854d87fb6b6a50f0626
Edgar Ravenhorst <edgarravenhorst@Edgars-iMac.local>
2016-01-07 16:17:46 +0100

    asd

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -29,7 +29,7 @@ var ApplicationController = Ember.Controller.extend({
 				type: "POST",
 				contentType: "application/json; charset=utf-8",
 				url: ENV.APP.API_HOST+'/'+ENV.APP.API_NS+"/actions/"+actionName,
-				data: JSON.stringify(params
+				data: JSON.stringify(params)
 			}).done(function(response){
 				resolve(response);
 			}).fail(function(error) {
