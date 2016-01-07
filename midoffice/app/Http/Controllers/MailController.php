0242485fceec918aaabaee806cc997e341e0a803
Edgar Ravenhorst <edgarravenhorst@Edgars-iMac.local>
2016-01-07 15:44:31 +0100

    asd

--- midoffice/app/Http/Controllers/MailController.php
+++ midoffice/app/Http/Controllers/MailController.php
@@ -8,53 +8,56 @@ use StdClass;
 class MailController extends BaseController
 {
 
-    public function testmail(){
-        $mail = Mail::send('emails.testmail', array(), function ($message) {
-            $message->from('no-reply@xtalus.nl', 'Xtalus');
-            $message->to('mathijs@code.rehab', "Code.Rehab");
-            $message->to('mathijsiej1987@gmail.com', "Code.Rehab");
-            $message->subject("Testmail");
-        });
+	public function testmail(){
+		$mail = Mail::send('emails.testmail', array(), function ($message) {
+			$message->from('no-reply@xtalus.nl', 'Xtalus');
+			$message->to('mathijs@code.rehab', "Code.Rehab");
+			$message->to('mathijsiej1987@gmail.com', "Code.Rehab");
+			$message->subject("Testmail");
+		});
 
-        $response = new StdClass();
-        $response->success = $mail;
+		$response = new StdClass();
+		$response->success = $mail;
 
-        return view('emails.testmail', array());
+		return view('emails.testmail', array());
 
-        return response()->json($response);
-    }
+		return response()->json($response);
+	}
 
-    public function sendmail(Request $request, $type, $subject){
+	public function sendmail(Request $request, $type, $subject){
 
-        $data = (object) $request->json('data');
+		$data = (object) $request->json('data');
 
-        if(isset($data->firstName) && isset($data->lastName))
-          $data->fullName = $data->firstName . ' ' .$data->lastName;
-        if(isset($data->middleName)) $data->fullName = $data->firstName . ' ' . $data->middleName . ' ' . $data->lastName;
+		var_dump($data);
 
 
-        $mail = Mail::send("emails.$type.$subject", array('postdata' => $data), function ($message) use($data) {
-            $message->from('no-reply@xtalus.nl', 'Xtalus');
-            $message->to("$data->email","$data->fullName");
-            $message->subject("$data->subject");
-        });
-
-        //return view("emails.$type.$subject", $request->json('data'));
-
-        $response = new StdClass();
-        $response->success = $mail;
-
-        return response()->json($response);
-    }
-
-    public function rendermail(Request $request, $type, $subject){
-
-        $data = (object) $request->input();
+		if(isset($data->firstName) && isset($data->lastName))
+			$data->fullName = $data->firstName . ' ' .$data->lastName;
+		if(isset($data->middleName)) $data->fullName = $data->firstName . ' ' . $data->middleName . ' ' . $data->lastName;
 
 
-        $data->fullName = $data->firstName . ' ' .$data->lastName;
-        if(isset($data->middleName)) $data->fullName = $data->firstName . ' ' . $data->middleName . ' ' . $data->lastName;
+		$mail = Mail::send("emails.$type.$subject", array('postdata' => $data), function ($message) use($data) {
+			$message->from('no-reply@xtalus.nl', 'Xtalus');
+			$message->to("$data->email","$data->fullName");
+			$message->subject("$data->subject");
+		});
 
-        return view("emails.$type.$subject", array('postdata' => $data));
-    }
+		//return view("emails.$type.$subject", $request->json('data'));
+
+		$response = new StdClass();
+		$response->success = $mail;
+
+		return response()->json($response);
+	}
+
+	public function rendermail(Request $request, $type, $subject){
+
+		$data = (object) $request->input();
+
+
+		$data->fullName = $data->firstName . ' ' .$data->lastName;
+		if(isset($data->middleName)) $data->fullName = $data->firstName . ' ' . $data->middleName . ' ' . $data->lastName;
+
+		return view("emails.$type.$subject", array('postdata' => $data));
+	}
 }
